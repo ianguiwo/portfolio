79d0d3dd2f589d51ecbe8c697d2b5ae8251abfe7
Ian Guiwo <ianguiwo@gmail.com>
2025-10-26 17:14:36 +0000

    fixed asset tag for resume page.js

--- app/resume/page.js
+++ app/resume/page.js
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { assets } from '@/assets/assets'
+import { assets } from '../../assets/assets'
 
 export default function Resume() {
   const [activeResume, setActiveResume] = useState('resume1')
