27c9be361df5de91cda2ebfed42d61911857a795
Ian Guiwo <ianguiwo@gmail.com>
2025-10-26 17:20:39 +0000

    removed document.js and added out layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,15 @@ import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["latin"], weight: ["400","500","600","700"],
-  variable: "--font-outfit"
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-outfit",
 });
 
 const ovo = Ovo({
-  subsets: ["latin"], weight: ["400"],
-  variable: "--font-ovo"
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-ovo",
 });
 
 export const metadata = {
@@ -18,9 +20,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body className={`${outfit.className} ${ovo.className} antialiased 
-      leading-8 overflow-x-hidden bg-blue-50`}>
+    <html lang="en" className="scroll-smooth dark">
+      <head>
+        {/* Tailwind CDN (only needed if Tailwind isn't built in) */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@4.1.14/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </head>
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden bg-blue-50`}
+      >
         {children}
       </body>
     </html>
